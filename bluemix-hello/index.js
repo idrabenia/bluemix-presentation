c6d1ea6ccc30fac9f01f10a622cb429a2fffaf92
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-06-20 11:46:26 +0300

    Add bluemix hello application

--- bluemix-hello/index.js
+++ bluemix-hello/index.js
@@ -5,5 +5,4 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 // Required port provided using environment variable PORT
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
